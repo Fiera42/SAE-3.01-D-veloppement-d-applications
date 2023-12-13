6874264f7923648ada3b672302fd3ae04936e51a
Fiera42 <anaigeon@gmx.fr>
2023-12-13 17:19:45 +0100

    text display

--- project/src/main/java/treulo/src/view/appview/display/TextDisplay.java
+++ project/src/main/java/treulo/src/view/appview/display/TextDisplay.java
@@ -1,8 +1,14 @@
 package treulo.src.view.appview.display;
 
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import treulo.src.model.TaskList;
 import treulo.src.model.TreuloTask;
+import javafx.scene.layout.
 
 import java.util.ArrayList;
 
@@ -16,30 +22,30 @@ public class TextDisplay implements Display {
 
     @Override
     public Node getDisplay() {
-        String res = "";
+        VBox vBox = new VBox();
 
         for(TaskList taskList : taskLists) {
-            res += "- " + taskList.getName() + " : \n";
+            VBox listVbox = new VBox();
             for(TreuloTask task : taskList) {
-                res += getTaskDisplay(task, new TextArea());
+                listVbox.getChildren().add(getTaskDisplay(task, new VBox()));
             }
+            vBox.getChildren().add(listVbox);
         }
-
-        TextArea textArea = new TextArea(res);
-        textArea.setEditable(false);
-        return textArea;
     }
 
     @Override
     public Node getTaskDisplay(TreuloTask task, Node parentNode) {
-        TextArea textArea = (TextArea)parentNode;
-        String res = textArea.getText();
+        VBox parent = (VBox)parentNode;
+        VBox vBox = new VBox();
 
-        res += "- " + task.getName() + "(" + task.getDescription() + ")";
+        Insets insets = parent.getPadding();
+        vBox.setPadding(new Insets(insets.getTop(), insets.getRight(), insets.getBottom(), insets.getLeft() + 5));
+
+        vBox.getChildren().add(new Label("-" + task.getName() + " (" + task.getDescription() + ")"));
         for(TreuloTask child : task.getSubtasks()) {
-            String childText = ((TextArea)getTaskDisplay(child, new TextArea())).getText();
-            res += " " + childText;
+            vBox.getChildren().add(getTaskDisplay(child, vBox));
         }
-        return
+
+        return vBox;
     }
 }
