bc24f7aa48c150dc5d0035f60c6b0fb95eb7c035
Fiera42 <anaigeon@gmx.fr>
2023-12-18 15:32:45 +0100

    squellette deskDisplay

---
+++ project/src/main/java/treulo/src/view/appview/display/DeskDisplay.java
@@ -0,0 +1,64 @@
+package treulo.src.view.appview.display;
+
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import treulo.src.Controler.EditTaskListControl;
+import treulo.src.model.TaskList;
+import treulo.src.model.Treulo;
+import treulo.src.model.TreuloTask;
+
+import java.util.LinkedList;
+
+public class DeskDisplay implements Display {
+
+    private LinkedList<TaskList> taskLists;
+    private Treulo model;
+
+
+    public DeskDisplay(Treulo treulo, LinkedList<TaskList> taskLists) {
+        this.model = treulo;
+        this.taskLists = taskLists;
+    }
+
+    @Override
+    public Node getDisplay() {
+        HBox hBox = new HBox();
+
+        for(TaskList taskList : taskLists) {
+            hBox.getChildren().add(getTaskListDisplay(taskList));
+        }
+
+        return hBox;
+    }
+
+    public Node getTaskListDisplay(TaskList taskList) {
+        VBox vBox = new VBox();
+        TextField listName = new TextField(taskList.getName());
+        listName.setOnAction(new EditTaskListControl(model, taskList));
+
+        for(TreuloTask task : taskList) {
+            listVbox.getChildren().add(getTaskDisplay(task, new VBox()));
+        }
+        return null;
+    }
+
+    @Override
+    public Node getTaskDisplay(TreuloTask task, Node parentNode) {
+        VBox parent = (VBox)parentNode;
+        VBox vBox = new VBox();
+
+        Insets insets = parent.getPadding();
+        vBox.setPadding(new Insets(insets.getTop(), insets.getRight(), insets.getBottom(), insets.getLeft() + 5));
+
+        vBox.getChildren().add(new Label("-" + task.getName() + " (" + task.getDescription() + ")"));
+        for(TreuloTask child : task.getSubtasks()) {
+            vBox.getChildren().add(getTaskDisplay(child, vBox));
+        }
+
+        return vBox;
+    }
+}
---
+++ project/src/main/java/treulo/src/view/appview/displayFactory/DeskDisplayFactory.java
@@ -0,0 +1,17 @@
+package treulo.src.view.appview.displayFactory;
+
+import treulo.src.model.Treulo;
+import treulo.src.view.appview.display.Display;
+import treulo.src.view.appview.display.TextDisplay;
+
+public class DeskDisplayFactory extends DisplayFactory {
+
+    private Treulo model;
+    public DeskDisplayFactory(Treulo model) {
+        this.model = model;
+    }
+    @Override
+    public Display createDisplay() {
+        return new TextDisplay(model, model.getTasks());
+    }
+}
