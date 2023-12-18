9364d65bbada69d6f83e9adaa5260ff3411e15e3
unknown <fouquettom2004@gmail.com>
2023-12-18 23:05:17 +0100

    taskmenu

--- project/src/main/java/treulo/src/Controler/AddTaskMenuControl.java
+++ project/src/main/java/treulo/src/Controler/AddTaskMenuControl.java
@@ -3,17 +3,18 @@ package treulo.src.Controler;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import treulo.src.model.Model;
+import treulo.src.model.Treulo;
 
 public class AddTaskMenuControl implements EventHandler<ActionEvent> {
-        Model model;
+        Treulo model;
 
         public AddTaskMenuControl(Model m)
         {
-            model=m;
+            model= (Treulo) m;
         }
 
     @Override
     public void handle(ActionEvent actionEvent) {
-
+        this.model.setDisplayMode("Nouvelle tache");
     }
 }
