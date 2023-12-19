b1d106f19e10df228853457f9917b0d0cdc146db
unknown <fouquettom2004@gmail.com>
2023-12-19 09:00:36 +0100

    addTaskControler

--- project/src/main/java/treulo/src/Controler/AddTaskControl.java
+++ project/src/main/java/treulo/src/Controler/AddTaskControl.java
@@ -1,13 +1,26 @@
 package treulo.src.Controler;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import treulo.src.model.Model;
 import treulo.src.model.TaskList;
+import treulo.src.model.Treulo;
+import treulo.src.model.TreuloTask;
 
-public class AddTaskControl {
-        Model model;
-
-        public AddTaskControl(Model m)
+public class AddTaskControl implements EventHandler <ActionEvent> {
+        Treulo model;
+        TaskList tL;
+        public AddTaskControl(Model m,TaskList tL)
         {
-            model=m;
+            model= (Treulo)m;
+            tL=tL;
         }
+
+    @Override
+    public void handle(ActionEvent event) {
+            TreuloTask treutask = new TreuloTask();
+
+            //tL.addTask();
+        this.model.setDisplayMode("Tableau");
+    }
 }
