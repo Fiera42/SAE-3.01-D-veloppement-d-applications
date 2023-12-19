cf5995308748561d29631c73ac9d7dc991745fe6
Flyguss <crafteur55100-verdun@hotmail.com>
2023-12-19 09:27:52 +0100

    Ajout de la fonctionnalité pour supprimer une liste de tache

--- project/src/main/java/treulo/src/Controler/DeleteTaskListControl.java
+++ project/src/main/java/treulo/src/Controler/DeleteTaskListControl.java
@@ -1,12 +1,23 @@
 package treulo.src.Controler;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import treulo.src.model.Model;
+import treulo.src.model.TaskList;
+import treulo.src.model.Treulo;
 
-public class DeleteTaskListControl {
-    Model model;
+public class DeleteTaskListControl implements EventHandler<ActionEvent> {
+    Treulo model;
+    TaskList taskList;
 
-    public DeleteTaskListControl(Model m)
+    public DeleteTaskListControl(Model m , TaskList tl)
     {
-        model=m;
+        model= (Treulo) m;
+        this.taskList = tl ;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        model.removeTaskList(taskList);
     }
 }
