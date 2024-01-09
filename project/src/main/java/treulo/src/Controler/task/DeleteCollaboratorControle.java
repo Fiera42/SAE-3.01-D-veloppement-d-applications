995f7b18719b8d1a0b209f7296da43a9c910bc00
Flyguss <crafteur55100-verdun@hotmail.com>
2024-01-09 10:24:52 +0100

    Possibilité de supprimer collaborateur et dependance dans l'affichage detaille

---
+++ project/src/main/java/treulo/src/Controler/task/DeleteCollaboratorControle.java
@@ -0,0 +1,38 @@
+package treulo.src.Controler.task;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import treulo.src.model.Treulo;
+import treulo.src.model.TreuloTask;
+
+public class DeleteCollaboratorControle implements EventHandler<ActionEvent> {
+
+    Treulo model;
+    TreuloTask treuloTask ;
+    String collabo;
+
+    public DeleteCollaboratorControle(Treulo m, String s , TreuloTask task) {
+        this.model = m ;
+        this.collabo = s ;
+        this.treuloTask = task ;
+
+    }
+
+    public DeleteCollaboratorControle(Treulo m, String s) {
+        this.model = m ;
+        this.collabo = s ;
+        this.treuloTask = null ;
+
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+        if (this.model.getCollaboratorTempo().contains(collabo)) {
+            this.model.getCollaboratorTempo().remove(collabo);
+        }else {
+            this.treuloTask.getCollaborators().remove(collabo);
+        }
+        this.model.updateObservator();
+    }
+}
